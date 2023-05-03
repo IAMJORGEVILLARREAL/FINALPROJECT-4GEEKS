3814aeca90f5d88c4ce6e98fe0f3a1155f3e7d9f
arixvi <122501401+arixvi@users.noreply.github.com>
2023-05-02 20:27:08 -0600

    Update home.js

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,18 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+  <div className="container">
+    <div className="jumbotron">
+      <h1 className="display-4">Hello, world!</h1>
+      <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr className="my-4"/>
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <p className="lead">
+        <Link className="nav-link active" to="/about" >
+          <button className="btn btn-primary btn-custom" href="#" role="button">Learn more</button>
+        </Link>
+      </p>
+    </div>
+  </div>
 );
