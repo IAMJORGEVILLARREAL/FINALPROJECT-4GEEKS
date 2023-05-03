4b8787ed0a24f34804abe7c43ed2b17e3a12edc8
arixvi <122501401+arixvi@users.noreply.github.com>
2023-05-02 20:20:50 -0600

    Update navbar.js

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,29 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">REALM32</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <Link className="nav-link active" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to="/services">Services</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to="/about">About Us</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 	);
 };
