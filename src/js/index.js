23cc79416f0006785f19e2b6b10c15a32ea1e9b7
arixvi <122501401+arixvi@users.noreply.github.com>
2023-05-02 20:43:23 -0600

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -8,9 +8,11 @@ import "../styles/index.css";
 //import your own components
 import Layout from './layout.js'
 
+
+
+
 //
 const root = createRoot(document.querySelector("#app"))
 
 //render your react application
 root.render(<Layout/>)
-
